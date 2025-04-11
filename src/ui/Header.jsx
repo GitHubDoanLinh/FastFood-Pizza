74ac68abec529b5770c2284782054a77dd56ad10
Doan Huu Linh <doanlinh1510@gmail.com>
2025-04-11 18:53:12 +0700

    Fetching Orders

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
+import SearchOrder from "../features/order/SearchOrder";
 
 function Header() {
   return (<header>
     <Link to={"/"}>FastFood React Co.</Link>
+    <SearchOrder />
   </header>)
 }
 
